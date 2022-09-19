5eeb49068620a8156212d60ecd36765e6fa0d246
Lukas Engel <lukas.engel@protonmail.com>
2022-09-19 23:31:26 +0200

    Remove unused import

--- lib/pages/edit_page/edit_page.dart
+++ lib/pages/edit_page/edit_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_connect/http/src/utils/utils.dart';
 
 import '../../models/school_life_item.dart';
 import './edit_page_controller.dart';
